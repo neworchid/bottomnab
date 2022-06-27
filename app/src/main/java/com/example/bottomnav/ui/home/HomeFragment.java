336923220baaad0daa4872877a993580d2b789fe
fdlnwknd <fadhlan@weekendinc.com>
2022-06-28 00:40:38 +0700

    init

---
+++ app/src/main/java/com/example/bottomnav/ui/home/HomeFragment.java
@@ -0,0 +1,50 @@
+package com.example.bottomnav.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.bottomnav.SecAct;
+import com.example.bottomnav.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+  private FragmentHomeBinding binding;
+
+  public View onCreateView(@NonNull LayoutInflater inflater,
+                           ViewGroup container, Bundle savedInstanceState) {
+    HomeViewModel homeViewModel =
+        new ViewModelProvider(this).get(HomeViewModel.class);
+
+    binding = FragmentHomeBinding.inflate(inflater, container, false);
+    View root = binding.getRoot();
+
+    final TextView textView = binding.textHome;
+    homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+    final Button button = binding.btnSecactivity;
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(root.getContext(), SecAct.class);
+        startActivity(intent);
+      }
+    });
+    return root;
+  }
+
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
+}
